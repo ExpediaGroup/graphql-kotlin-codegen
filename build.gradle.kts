68375af37d3d6151d1bac0b336389a025d936a3f
Dan Adajian <dadajian@expediagroup.com>
2024-03-06 15:29:02 -0600

    update docs and add maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+repositories {
+    mavenCentral()
+}
+
 plugins {
     kotlin("jvm") version "1.9.21"
 }
 
-val graphqlKotlinVersion = "7.0.2"
 dependencies {
-    implementation("com.expediagroup", "graphql-kotlin-schema-generator", graphqlKotlinVersion)
-    implementation("com.expediagroup", "graphql-kotlin-federation", graphqlKotlinVersion)
+    implementation("com.expediagroup", "graphql-kotlin-schema-generator", "7.0.2")
+    implementation("com.expediagroup", "graphql-kotlin-federation", "7.0.2")
 }
 
 sourceSets {
