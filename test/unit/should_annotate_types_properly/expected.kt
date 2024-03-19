b7c8bce72754c5209f5e299bc056a5ef3dcac28f
Dan Adajian <dadajian@expediagroup.com>
2024-03-19 11:09:35 -0500

    fix: avoid using @Deprecated on union type

--- test/unit/should_annotate_types_properly/expected.kt
+++ test/unit/should_annotate_types_properly/expected.kt
@@ -8,5 +8,26 @@ data class MyType(
     @GraphQLDescription("A description for email")
     val email: String? = null,
     @GraphQLDescription("A `weird` description for name")
-    val name: String? = null
+    val name: String? = null,
+    @Deprecated("Use something else instead")
+    val deprecated1: String? = null,
+    @Deprecated("")
+    val deprecated2: String? = null,
+    @Deprecated("Deprecated directive works too")
+    val deprecated3: String? = null,
+    @Deprecated("It only takes the first one")
+    val deprecated4: String? = null,
+    @MyUnion
+    @GraphQLDescription("DEPRECATED: It uses the GraphQLDescription annotation for union types")
+    val deprecated5: Any? = null,
+    @MyUnion
+    @GraphQLDescription("It uses the GraphQLDescription annotation for union types")
+    val deprecated6: Any? = null
 )
+
+@GraphQLUnion(
+    name = "MyUnion",
+    possibleTypes = [MyType::class],
+    description = ""
+)
+annotation class MyUnion
