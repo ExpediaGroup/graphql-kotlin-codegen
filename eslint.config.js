bd1b540a1ec40ed489f3ed61a93e0b38029ce925
Dan Adajian <dadajian@expediagroup.com>
2024-04-22 13:56:58 -0500

    migrate config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+import typescriptEslint from "typescript-eslint";
+
+export default [
+  ...typescriptEslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+    },
+  },
+  {
+    ignores: ["build", "dist", "docs"],
+  },
+];
