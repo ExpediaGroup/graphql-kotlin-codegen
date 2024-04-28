6e44e9efd89438809be54601c7f2665113029a90
Dan Adajian <dadajian@expediagroup.com>
2024-04-28 15:30:23 -0500

    unit test

--- test/unit/should_generate_field_resolver_interfaces/expected.kt
+++ test/unit/should_generate_field_resolver_interfaces/expected.kt
@@ -2,54 +2,42 @@ package com.kotlin.generated
 
 import com.expediagroup.graphql.generator.annotations.*
 
-@GraphQLIgnore
-interface Query {
-    suspend fun nullableField(): FieldType? = null
-    suspend fun nonNullableField(): FieldType
-    suspend fun nullableResolver(arg: String): String? = null
-    suspend fun nonNullableResolver(arg: InputTypeGenerateFieldResolverInterfaces): String
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class TypeWithOnlyFieldArgs {
+    open fun nullableResolver(arg: String): String? = throw NotImplementedError("Query.nullableResolver must be implemented.")
+    open fun nonNullableResolver(arg: InputTypeForResolver): String = throw NotImplementedError("Query.nonNullableResolver must be implemented.")
 }
 
-@GraphQLIgnore
-interface QueryCompletableFuture {
-    fun nullableField(): java.util.concurrent.CompletableFuture<FieldType?>
-    fun nonNullableField(): java.util.concurrent.CompletableFuture<FieldType>
-    fun nullableResolver(arg: String): java.util.concurrent.CompletableFuture<String?>
-    fun nonNullableResolver(arg: InputTypeGenerateFieldResolverInterfaces): java.util.concurrent.CompletableFuture<String>
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class HybridType(
+    val nullableField: String? = null,
+    val nonNullableField: String
+) {
+    open fun nullableResolver(arg: String): String? = throw NotImplementedError("Query.nullableResolver must be implemented.")
+    open fun nonNullableResolver(arg: InputTypeForResolver): String = throw NotImplementedError("Query.nonNullableResolver must be implemented.")
 }
 
 @GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.INPUT_OBJECT])
-data class InputTypeGenerateFieldResolverInterfaces(
+data class InputTypeForResolver(
     val field: String? = null
 )
 
-interface MyFieldInterface {
-    suspend fun field1(): String?
-    suspend fun field2(): String
-    suspend fun nullableListResolver(arg1: Int?, arg2: Int): List<String?>?
-    suspend fun nonNullableListResolver(arg1: Int, arg2: Int?): List<String>
+interface HybridInterface {
+    val field1: String?
+    val field2: String
+    fun nullableListResolver(arg1: Int?, arg2: Int): List<String?>?
+    fun nonNullableListResolver(arg1: Int, arg2: Int?): List<String>
 }
 
-@GraphQLIgnore
-interface FieldType : MyFieldInterface {
-    override suspend fun field1(): String? = null
-    override suspend fun field2(): String
-    suspend fun booleanField1(): Boolean? = null
-    suspend fun booleanField2(): Boolean = false
-    suspend fun integerField1(): Int? = null
-    suspend fun integerField2(): Int
-    override suspend fun nullableListResolver(arg1: Int?, arg2: Int): List<String?>? = null
-    override suspend fun nonNullableListResolver(arg1: Int, arg2: Int?): List<String> = emptyList()
-}
-
-@GraphQLIgnore
-interface FieldTypeCompletableFuture {
-    fun field1(): java.util.concurrent.CompletableFuture<String?>
-    fun field2(): java.util.concurrent.CompletableFuture<String>
-    fun booleanField1(): java.util.concurrent.CompletableFuture<Boolean?>
-    fun booleanField2(): java.util.concurrent.CompletableFuture<Boolean>
-    fun integerField1(): java.util.concurrent.CompletableFuture<Int?>
-    fun integerField2(): java.util.concurrent.CompletableFuture<Int>
-    fun nullableListResolver(arg1: Int?, arg2: Int): java.util.concurrent.CompletableFuture<List<String?>?>
-    fun nonNullableListResolver(arg1: Int, arg2: Int?): java.util.concurrent.CompletableFuture<List<String>>
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class TypeImplementingInterface(
+    override val field1: String? = null,
+    override val field2: String,
+    val booleanField1: Boolean? = null,
+    val booleanField2: Boolean,
+    val integerField1: Int? = null,
+    val integerField2: Int
+) : HybridInterface {
+    override fun nullableListResolver(arg1: Int?, arg2: Int): List<String?>? = throw NotImplementedError("FieldType.nullableListResolver must be implemented.")
+    override fun nonNullableListResolver(arg1: Int, arg2: Int?): List<String> = throw NotImplementedError("FieldType.nonNullableListResolver must be implemented.")
 }
