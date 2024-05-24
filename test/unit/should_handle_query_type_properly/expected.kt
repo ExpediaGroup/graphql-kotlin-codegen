0c5070f42807a8e0bc70b86762482b307b1df908
Dan Adajian <dadajian@expediagroup.com>
2024-05-24 14:11:30 -0500

    unit test

---
+++ test/unit/should_handle_query_type_properly/expected.kt
@@ -0,0 +1,19 @@
+package com.kotlin.generated
+
+import com.expediagroup.graphql.generator.annotations.*
+
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class Query {
+    open fun getStuff(): String = throw NotImplementedError("Query.getStuff must be implemented.")
+    open fun getStuffWithInput(input: String, dataFetchingEnvironment: graphql.schema.DataFetchingEnvironment): String = throw NotImplementedError("Query.getStuffWithInput must be implemented.")
+}
+
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class GetStuffQuery {
+    open fun getStuff(): String = throw NotImplementedError("Query.getStuff must be implemented.")
+}
+
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+open class GetStuffWithInputQuery {
+    open fun getStuffWithInput(input: String, dataFetchingEnvironment: graphql.schema.DataFetchingEnvironment): String = throw NotImplementedError("Query.getStuffWithInput must be implemented.")
+}
