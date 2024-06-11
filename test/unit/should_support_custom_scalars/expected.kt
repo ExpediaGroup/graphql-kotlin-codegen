88e0287396ae9b4cf66ca39cb58e178463afcb57
Dan Adajian <dadajian@expediagroup.com>
2024-06-11 08:46:03 -0500

    unit test

--- test/unit/should_support_custom_scalars/expected.kt
+++ test/unit/should_support_custom_scalars/expected.kt
@@ -6,11 +6,13 @@ import com.expediagroup.graphql.generator.annotations.*
 data class MyScalarType(
     val idField: com.expediagroup.graphql.generator.scalars.ID? = null,
     val field: java.net.URL? = null,
-    val field2: java.net.URL
+    val field2: java.net.URL,
+    val field3: List<java.net.URL> = emptyList()
 )
 
 @GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
 data class MyCustomScalarType(
     val field: String? = null,
-    val field2: String
+    val field2: String,
+    val field3: List<String> = emptyList()
 )
