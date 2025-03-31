9237118b31313fbcd85f25f1da0a51ed80020f90
Dan Adajian <dadajian@expediagroup.com>
2025-03-31 13:46:26 -0500

    add test

--- test/unit/should_honor_includeDependentTypes_false_for_external_unions_and_interfaces/expected.kt
+++ test/unit/should_honor_includeDependentTypes_false_for_external_unions_and_interfaces/expected.kt
@@ -8,6 +8,14 @@ data class MyIncludedType(
     val field2: String
 )
 
+@GraphQLValidObjectLocations(locations = [GraphQLValidObjectLocations.Locations.OBJECT])
+data class MyIncludedType2(
+    val field: String? = null,
+    val field2: String
+) : MyIncludedUnion
+
+interface MyIncludedUnion
+
 interface MyIncludedInterfaceInOnlyTypes {
     val field: String?
     val field2: String
